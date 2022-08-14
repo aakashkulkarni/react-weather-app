09cc0a5e49ec80185263e46e7fa593f0a1b08b56
aakashkulkarni <aakash.utah@gmail.com>
2022-08-14 01:42:39 -0600

    Added forecast component and styles to current weather and forecasts

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6388290841mshc786b03c9ef4587p11eafcjsnba26c1d22f5d",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "30ec98be8d98e8fa39d6152246d83ac1";
